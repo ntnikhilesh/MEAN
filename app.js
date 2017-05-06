af8921fe7fe73a50cb8b9ee7c4444b445299c6a1
nikhil <ntnikhilesh@gmail.com>
2017-05-06 17:06:47 +0530

    first commit- Express setup and Routes

---
+++ app.js
@@ -0,0 +1,50 @@
+const express=require('express');
+const path=require('path');
+const bodyParser=require('body-parser');
+const cors=require('cors');
+const passport=require('passport');
+const mongoose=require('mongoose');
+const config =require('./config/database');
+
+//connect to database
+mongoose. connect(config.database);
+
+//on connection
+mongoose.connection.on('connected',()=>{
+    console.log('Connected to database'+config.database);
+});
+
+//on error
+mongoose.connection.on('error',(err)=>{
+    console.log(' Database error'+err);
+});
+
+const app=express();
+
+//define routes in diff file
+const users=require('./routes/users');
+
+//port no
+const port=3000;
+
+//Set Static folder
+
+app.use(express.static(path.join(__dirname,'public')));
+
+// Body  parser Middleware
+app.use(bodyParser.json());
+
+//allow all domain access
+app.use(cors())
+
+//add route
+
+app.get('/',(req,res)=>{
+    res.send('Invalid end point');
+})
+
+app.use('/users',users);
+
+app.listen(port,()=>{
+    console.log('Server started on port '+port);
+});
