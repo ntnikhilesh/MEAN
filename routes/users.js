1d0ce1d5f05f8a99fad7329d889150adcc7c8de3
nikhil <ntnikhilesh@gmail.com>
2017-05-07 01:01:01 +0530

    login and logut

--- routes/users.js
+++ routes/users.js
@@ -37,7 +37,7 @@ router.post('/authenticate',(req,res,next)=>{
         if(err) throw err;
         if(!user)
         {
-            return res.json({succuss:false,msg:'User not found'});
+            return res.json({success:false,msg:'User not found'});
         }
 
         User.comparePassword(password,user.password,(err,isMatch)=>{
@@ -49,7 +49,7 @@ router.post('/authenticate',(req,res,next)=>{
                 });
 
                 res.json({
-                    succuss:true,
+                    success:true,
                     token:'JWT '+token,
                     user:{
                         id:user._id,
@@ -59,7 +59,7 @@ router.post('/authenticate',(req,res,next)=>{
                     }
                 });
             }else{
-                return res.json({succuss:false,msg:'Wrong password'});
+                return res.json({success:false,msg:'Wrong password'});
             }
         });
     });
